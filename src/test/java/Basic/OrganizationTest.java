00688a0d35b766ed5bf16b26564b13580afbdc8c
SHIVANI127-hash <shivani.patil.mech.2017@vpkbiet.org>
2024-03-18 18:03:25 +0530

    added

--- src/test/java/Basic/OrganizationTest.java
+++ src/test/java/Basic/OrganizationTest.java
@@ -1,14 +1,26 @@
 package Basic;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
 
 public class OrganizationTest {
 
 	@Test
-	public void CreateOrganizationTest() {
-		 ChromeDriver driver = new ChromeDriver();
-		 driver.get("http://localhost:8888/");
-		 }
+	public void CreateOrganizationTest() throws IOException {
+            ChromeDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get("http://localhost:8888/");
+		    //login to application
+		    driver.findElement(By.name("user_name")).sendKeys("admin");
+		    driver.findElement(By.name("user_password")).sendKeys("admin");
+		    driver.findElement(By.id("submitButton")).click();
+		    	
+	}
 }
 
